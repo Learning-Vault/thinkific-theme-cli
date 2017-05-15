cfe1baaf4d76a65c3c68d931a3d28879afe40a2d
Jesus Rodriguez <opposite-bracket@opposite.bracket@gmail.com>
2017-05-15 16:10:15 -0700

    test wizard validation

--- test/commands/wizard.spec.js
+++ test/commands/wizard.spec.js
@@ -1,10 +1,29 @@
 'use strict'
 
 var should = require('should');
+var rewire = require('rewire');
 var wizard = rewire('../../src/commands/wizard');
 
 describe('wizard command', function() {
   it('should retrieve a module', function() {
     should.exist(wizard);
+  });
+
+  it('shouldn\' throw errors when valid subcommands are passed', function() {
+    should.doesNotThrow(function(){
+      wizard.options.validate_args(['setup'])
+    });
+  });
+
+  it('should throw errors when invalid subcommands are passed', function() {
+    should.throws(function(){
+      wizard.options.validate_args(['invalid']);
+    })
+  });
+
+  it('should throw errors when no subcommands are passed', function() {
+    should.throws(function(){
+      wizard.options.validate_args([]);
+    })
   });
 });
