1d6c9e3be6ed5cc041a8b5df426502a1c40939e1
Jesus Rodriguez <opposite-bracket@opposite.bracket@gmail.com>
2017-05-16 16:06:19 -0700

    fix nasty tests

--- test/services/themes.spec.js
+++ test/services/themes.spec.js
@@ -1,9 +1,7 @@
 'use strict'
 
 var should = require('should');
-var readline = require('readline');
-var rewire = require('rewire');
-var themes = rewire('../../src/commands/themes');
+var themes = require('../../src/core/services/themes');
 var helpers = require('../../src/core/helpers');
 
 describe('themes command', function() {
@@ -14,19 +12,19 @@ describe('themes command', function() {
 
   it('shouldn\' throw errors when valid subcommands are passed', function() {
     should.doesNotThrow(function(){
-      themes.options.validate_args(['list']);
+      wizard.options.validate_args(['list']);
     });
   });
 
   it('should throw errors when invalid subcommands are passed', function() {
     should.throws(function(){
-      themes.options.validate_args(['invalid']);
+      wizard.options.validate_args(['invalid']);
     })
   });
 
   it('should throw errors when no subcommands are passed', function() {
     should.throws(function(){
-      themes.options.validate_args([]);
+      wizard.options.validate_args([]);
     })
   });
 
