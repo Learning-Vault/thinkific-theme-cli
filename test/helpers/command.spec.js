fef511dde3087d0ce03bb26dcf5dc8e292da778d
Jesus Rodriguez <opposite-bracket@opposite.bracket@gmail.com>
2017-05-19 16:41:31 -0700

    Implement more config tests

---
+++ test/helpers/command.spec.js
@@ -0,0 +1,6 @@
+// const should = require('should');
+// const commandHelpers = require('../../src/helpers/command');
+
+// describe('command Helpers', () => {
+
+// });
--- test/helpers/config.spec.js
+++ test/helpers/config.spec.js
@@ -2,16 +2,33 @@ const should = require('should');
 const rewire = require('rewire');
 const path = require('path');
 const confiSample = require('../fixtures/config-sample.json');
+
 const configHelpers = rewire('../../src/helpers/config');
 
 describe('command Helpers', () => {
   it('should load credentials if they exist', () => {
     const getConfigPath = () => path.resolve(__dirname, '../fixtures/config-sample.json');
     configHelpers.__set__('getConfigPath', getConfigPath);
-    configHelpers.getConfigData().should.deepEqual(confiSample);
+    should(configHelpers.getConfigData()).deepEqual(confiSample);
   });
+
   it('should load empty credentials path doesn\'t exist', () => {
     configHelpers.__set__('getConfigPath', () => ('invalid-path'));
-    configHelpers.getConfigData().should.deepEqual({});
+    should(configHelpers.getConfigData()).deepEqual({});
+  });
+
+  it('should serialize credentials upon write execution', () => {
+    const expectedPath = path.resolve(__dirname, '../fixtures/config-sample.json');
+    const expectedData = { hello: 'world' };
+    const fs = {
+      writeFile: (configPath, data) => {
+        should(configPath).be.equal(expectedPath);
+        should(data).be.equal(expectedData);
+      },
+    }
+    const getConfigPath = () => expectedPath;
+    configHelpers.__set__('getConfigPath', getConfigPath);
+    configHelpers.__set__('fs', fs);
+    should(configHelpers.setConfigData(expectedData, () => {}))
   });
 });
