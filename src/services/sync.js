f54fb8bbb8b736ad4a8cc62b8bd255f5f2847e74
Jesus Rodriguez <opposite-bracket@opposite.bracket@gmail.com>
2017-05-23 18:00:53 -0700

    Implement function that understands what to do with the file

--- src/services/sync.js
+++ src/services/sync.js
@@ -4,23 +4,23 @@ const print = require('../print');
 const chalk = require('chalk');
 let configHelpers = require('../helpers/config'); // eslint-disable-line prefer-const
 
-// const syncFile = (eventType, themePath, filename) => {
-//   const resource = filename.replace(`${filename}/`, '');
-//   switch (eventType) {
-//     case 'add':
-//       print(chalk.green(`File Created ${resource}\n`));
-//       break;
-//     case 'change':
-//       print(chalk.green(`File Changed ${resource}\n`));
-//       break;
-//     case 'unlink':
-//       print(chalk.green(`File Deleted ${resource}\n`));
-//       break;
-//     default:
-//       print(chalk.red(`didn't understand ${resource} event\n`));
-//       break;
-//   }
-// };
+const syncFile = (eventType, themePath, filename) => {
+  const resource = filename.replace(`${filename}/`, '');
+  switch (eventType) {
+    case 'add':
+      print(chalk.green(`File Created ${resource}\n`));
+      break;
+    case 'change':
+      print(chalk.green(`File Changed ${resource}\n`));
+      break;
+    case 'unlink':
+      print(chalk.green(`File Deleted ${resource}\n`));
+      break;
+    default:
+      print(chalk.red(`didn't understand ${resource} event\n`));
+      break;
+  }
+};
 
 const run = (themeId) => {
   const config = configHelpers.getConfigData();
@@ -30,6 +30,7 @@ const run = (themeId) => {
     throw Error('Theme doesn\'t exist!');
   }
   const themePath = path.resolve(config.path, config.themes[themeId]);
+  let ready = false;
   print(`Watching File Structure: ${chalk.bold(themePath)}!!\n`);
   print('Go and make Magic!\n');
   print('------------------\n');
@@ -37,31 +38,10 @@ const run = (themeId) => {
     filter: /\.liquid$/,
     recursive: true,
   })
-  // }).on('add', filename => syncFile('add', themePath, filename))
-  //   .on('change', filename => syncFile('change', themePath, filename))
-  //   .on('unlink', filename => syncFile('unlink', themePath, filename))
-    .on('raw', (event, filePath, details) => {
-      console.log(`
-Raw event info:
----------------
-
-Event: ${event}
-File Path: ${filePath}
-Details:
-{
-   "path": "${details.path}",
-   "event":"${details.event}",
-   "type":"${details.file}",
-   "changes":{
-      "inode": ${details.changes.inode},
-      "finder": ${details.changes.finder},
-      "access": ${details.changes.access},
-      "xattrs": ${details.changes.xattrs}
-   },
-   "flags": ${details.flags}
-}
-`);
-    });
+    .on('ready', () => (ready = true))
+    .on('add', (filename) => { if (ready) syncFile('add', themePath, filename); })
+    .on('change', (filename) => { if (ready) syncFile('change', themePath, filename) })
+    .on('unlink', (filename) => { if (ready) syncFile('unlink', themePath, filename) });
 };
 
 module.exports = {
